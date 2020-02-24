e169a6d6c1205cea642bc05d7fc5982db5697e28
Rob Hess <robwhess@gmail.com>
2020-02-24 14:48:01 -0800

    Add code from 2/24 lecture.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import AddTodo from './components/AddTodo';
 import TodoList from './components/TodoList';
 import VisibilityFilterButtons from './components/VisibilityFilterButtons';
+import { getActiveTodoCount } from './redux/selectors';
 
-const todos = [
-  {
-    text: 'Eat lunch',
-    completed: true,
-    id: 1
-  },
-  {
-    text: 'Research Redux',
-    completed: true,
-    id: 2
-  },
-  {
-    text: 'Finish app',
-    completed: false,
-    id: 3
-  }
-];
+// const todos = [
+//   {
+//     text: 'Eat lunch',
+//     completed: true,
+//     id: 1
+//   },
+//   {
+//     text: 'Research Redux',
+//     completed: true,
+//     id: 2
+//   },
+//   {
+//     text: 'Finish app',
+//     completed: false,
+//     id: 3
+//   }
+// ];
 
 function App() {
-  const activeTodoCount = todos.reduce(
-    (count, todo) => todo.completed ? count : count + 1,
-    0
-  );
+  const activeTodoCount = useSelector(getActiveTodoCount);
   return (
     <div>
       <h1>Todos ({activeTodoCount} active)</h1>
       <VisibilityFilterButtons />
       <AddTodo />
-      <TodoList todos={todos} />
+      <TodoList />
     </div>
   );
 }
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,5 +1,29 @@
+import { VisibilityFilters } from './actions';
+
 export function getTodos(state) {
   return state.todos;
+}
+
+export function getTodosByVisibilityFilter(state) {
+  const todos = getTodos(state);
+  const visibilityFilter = getVisibilityFilter(state);
+  switch (visibilityFilter) {
+    case VisibilityFilters.SHOW_COMPLETED:
+      return todos.filter(todo => todo.completed);
+    case VisibilityFilters.SHOW_ACTIVE:
+      return todos.filter(todo => !todo.completed);
+    case VisibilityFilters.SHOW_ALL:
+    default:
+      return todos;
+  }
+}
+
+export function getActiveTodoCount(state) {
+  const todos = getTodos(state);
+  return todos.reduce(
+    (count, todo) => todo.completed ? count : count + 1,
+    0
+  );
 }
 
 export function getVisibilityFilter(state) {
